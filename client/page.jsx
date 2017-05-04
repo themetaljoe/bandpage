e571fff0376a3b9f178d47767a91f70cf9f939b0
Joseph Praetorius <joepraetorius@Josephs-MacBook-Pro.local>
2017-05-03 22:18:50 -0500

    Added header componet

--- client/page.jsx
+++ client/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './header';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div className="page">
+            <Header />
 
       </div>
     );
