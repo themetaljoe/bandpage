f0dabe94a6393a672159d4dbd331e53bffd5adc0
John Rodney <johnrodney@Johns-MacBook-Pro.local>
2017-05-04 01:39:19 -0500

    m

--- client/events.jsx
+++ client/events.jsx
@@ -27,14 +27,17 @@ export default class events extends React.Component {
             return i < 5;
           })
           .map((event) => {
+            console.log(event)
             return (
               <div className='an-event'>
-                 <div className='message'>
-                  {
-                    moment(event.created_time).format('MMM Do')
-                  }:
-                  { ' ' + event.message.split('').filter((letter, i) => i < 200).join('') }
-                </div>
+                <a href={`https://www.facebook.com/carrythestorm/posts/${event.id}`}>
+                  <div className='message'>
+                    {
+                      moment(event.created_time).format('MMM Do')
+                    }:
+                    { ' ' + event.message.split('').filter((letter, i) => i < 200).join('') }
+                  </div>
+                </a>
               </div>
             );
           })
--- server/main.js
+++ server/main.js
@@ -6,7 +6,6 @@ Meteor.methods({
   getEvents() {
     return new Promise((resolve, reject) => {
       FacebookApi.get('/carrythestorm/posts?access_token=765736816923613|bb7bbe80b14f5b7240a6118c66aad28e&limit=100', (err, res) => {
-        console.log('called', err, res)
         resolve({err, res});
       });
     });
