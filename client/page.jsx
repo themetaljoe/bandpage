41525eda94364611852c7f1ecf968fbf2729c3e5
John Rodney <johnrodney@Johns-MacBook-Pro.local>
2017-05-04 05:31:38 -0500

    style updates w/ media queries

--- client/page.jsx
+++ client/page.jsx
@@ -3,21 +3,22 @@ import Header from './header';
 import Events from './events';
 import Images from './images';
 import SpotifyPlayer from 'react-spotify-player';
- 
-// size may also be a plain string using the presets 'large' or 'compact' 
+
+// size may also be a plain string using the presets 'large' or 'compact'
 const size = {
   width: '80%',
   height: 80,
 };
-const view = 'list'; // or 'coverart' 
-const theme = 'black'; // or 'white' 
- 
+const view = 'list'; // or 'coverart'
+const theme = 'black'; // or 'white'
+
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <div className="mask" />
+        <div className="gradient" />
         <div className="page">
           <Header />
           <SpotifyPlayer
