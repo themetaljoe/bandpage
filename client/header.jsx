670a3cc4d2d8a2b63afaac8c825a31c9ed48cc28
Joseph Praetorius <joepraetorius@Josephs-MacBook-Pro.local>
2017-05-04 01:40:48 -0500

    Social buttons

--- client/header.jsx
+++ client/header.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
 export default class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      socialIcons: [{ 
+        name: 'facebook', href: 'https://www.facebook.com/carrythestorm/'
+      },{ 
+        name: 'twitter', href: 'https://www.twitter.com/carrythestorm/'
+      },{ 
+        name: 'instagram', href: 'https://www.instagram.com/carrythestorm/'
+      },{ 
+        name: 'youtube', href: 'https://www.youtube.com/channel/UCe1IFN0qfZcBT4DDuugdKdw'
+      }]
+    }
+  }
   render() {
     return (
-      <div className="logo">  
-        <img src="logo.png" />    
-      </div>
+      <div>
+        <div className="logo">  
+          <img src="logo.png" />    
+        </div>
+            <div className="icons">
+        {
+          this.state.socialIcons.map((icon) => (
+            <div className="social-icon">
+              <a className={`btn btn-social-icon btn-${icon.name}`} href={icon.href}>
+              <span className={`fa fa-lg fa-${icon.name}`}></span>
+              </a>
+            </div>
+          ))
+        } 
+            </div>
+      </div>    
     );
   }
 }
