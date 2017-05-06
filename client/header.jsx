af0ab908c69e2f8442d1462c7fddec748e7f96fc
Marcus Chevalier <CueBall@Marcuss-MacBook-Pro.local>
2017-05-06 18:48:31 -0500

    Link Email

--- client/header.jsx
+++ client/header.jsx
@@ -4,26 +4,26 @@ export default class Header extends React.Component {
   constructor() {
     super();
     this.state = {
-      socialIcons: [{ 
+      socialIcons: [{
         name: 'facebook', href: 'https://www.facebook.com/carrythestorm/'
-      },{ 
+      },{
         name: 'twitter', href: 'https://www.twitter.com/carrythestorm/'
-      },{ 
+      },{
         name: 'instagram', href: 'https://www.instagram.com/carrythestorm/'
-      },{ 
+      },{
         name: 'youtube', href: 'https://www.youtube.com/channel/UCe1IFN0qfZcBT4DDuugdKdw'
       },{
         name: 'music', href: 'https://itunes.apple.com/us/artist/carry-the-storm/id412986445'
       },{
-        name: 'paper-plane', href: '#contact'
+        name: 'paper-plane', href: 'window.location.href = "mailto:carrythestorm@yahoo.com";'
       }]
     }
   }
   render() {
-    return (  
+    return (
       <div>
-        <div className="logo">  
-          <img src="logo.png" />    
+        <div className="logo">
+          <img src="logo.png" />
         </div>
         <h1 className='title-header'>New EP out Soon</h1>
             <div className="icons">
@@ -35,9 +35,9 @@ export default class Header extends React.Component {
               </a>
             </div>
           ))
-        } 
+        }
             </div>
-      </div>    
+      </div>
     );
   }
 }
