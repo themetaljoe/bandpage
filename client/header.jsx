4e7c28c3ad3b101526fc964301bd9d0703c78717
Joseph Praetorius <joepraetorius@Josephs-MacBook-Pro.local>
2017-05-06 19:02:17 -0500

    Marcus sucks

--- client/header.jsx
+++ client/header.jsx
@@ -15,7 +15,7 @@ export default class Header extends React.Component {
       },{
         name: 'music', href: 'https://itunes.apple.com/us/artist/carry-the-storm/id412986445'
       },{
-        name: 'paper-plane', href: 'window.location.href = "mailto:carrythestorm@yahoo.com";'
+        name: 'paper-plane', window.location.href = "mailto:carrythestorm@yahoo.com"
       }]
     }
   }
